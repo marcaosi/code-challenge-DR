e59464a6c6fa7f3670ce6d24c969cfcfb78166f5
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2021-11-08 20:03:44 -0300

    initial config for api

---
+++ api/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+const express = require('express')
+
+const app = express()
+
+// const routes = require('./src/routes')
+
+app.use(express.json())
+
+
+app.listen(PORT, err => console.log(err?.message || `API listening on port ${PORT}`))
