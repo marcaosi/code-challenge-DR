fd49ede4807c058b0f354f70522a5a6c20463103
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2021-11-11 14:03:15 -0300

    change port and add cors

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 const routes = require('./src/routes')
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 
